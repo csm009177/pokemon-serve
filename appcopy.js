af49227f5e2df70b026a47a1129448c6d038c2a9
csm <csm009177@gamil.com>
2023-10-05 15:43:21 +0900

    make multi page

--- appcopy.js
+++ appcopy.js
@@ -7,22 +7,42 @@ const server = http.createServer(function(request, response) {
   <head> <title>main</title>
   </head>  
   <body>  
-  <a href="http://localhost:1234/sub">sub</a>
+  <a href="http://localhost:1234/choi">choi</a>
+  <a href="http://localhost:1234/sung">sung</a>
+  <a href="http://localhost:1234/min">min</a>
   </body>  </html>`
 
-  let sub = `<!DOCTYPE html>
+  let choi = `<!DOCTYPE html>
   <html lang="en">
-  <head> <title>main</title>
+  <head> <title>choi</title>
+  </head>  <body>  <a href="http://localhost:1234">main</a>  </body>  </html>`
+  let sung = `<!DOCTYPE html>
+  <html lang="en">
+  <head> <title>sung</title>
+  </head>  <body>  <a href="http://localhost:1234">main</a>  </body>  </html>`
+  let min = `<!DOCTYPE html>
+  <html lang="en">
+  <head> <title>min</title>
   </head>  <body>  <a href="http://localhost:1234">main</a>  </body>  </html>`
 
 if(request.url==="/"){
   response.writeHead(200, {"Content-Type": "text/html"})
   response.end(main);
 }
-  if(request.url==="/sub") {
+  if(request.url==="/choi") {
     response.writeHead(200, {"Content-Type": "text/html"})
-    response.end(sub);
-  }
+    response.end(choi);
+}
+  if(request.url==="/sung") {
+    response.writeHead(200, {"Content-Type": "text/html"})
+    response.end(sung);
+}
+  if(request.url==="/min") {
+    response.writeHead(200, {"Content-Type": "text/html"})
+    response.end(min);
+}
+  
 });
+
 
 server.listen(1234);
